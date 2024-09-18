ef599d712e22a2c4b5db3f5225feb93996acbd7b
HoangNhatQuan <quanhn@metadatasolutions.vn>
2024-09-18 10:30:27 +0700

    _

--- src/app/[locale]/service/page.tsx
+++ src/app/[locale]/service/page.tsx
@@ -9,10 +9,14 @@ import Header from '@/components/Header'
 import { our_services } from '@/constants'
 import { coconatBold, coconatDemi } from '@/configs/fonts'
 import Hero from '@/components/Sections/Hero'
+import { useSearchParams } from 'next/navigation'
 
 export default function ServicePage() {
   const t = useTranslations()
-  const [active, setActive] = useState(0)
+  const current = useSearchParams().get('tab') || 'quick_healing'
+  const [active, setActive] = useState(current)
+  const index = our_services.findIndex((service) => service.key === current)
+
   return (
     <div className="flex flex-col w-full lg:snap-mandatory lg:snap-y overflow-y-auto h-auto lg:h-screen">
       <div className="lg:h-screen lg:shrink-0 lg:snap-start bg-service flex bg-cover h-auto pt-40 pb-8 lg:pt-[25dvh] px-6 flex-col gap-4 items-center relative">
@@ -31,15 +35,15 @@ export default function ServicePage() {
           {our_services.map((service, index) => (
             <button
               key={service.key}
-              onClick={() => setActive(index)}
-              className={`col-span-1 btn btn-outline text-white border-white ${active === index && 'bg-white !text-black'} hover:bg-white hover:text-black rounded-full btn-xs lg:btn-md text-sm lg:text-base px-4 py-2 lg:!px-5 lg:!py-0 h-auto`}
+              onClick={() => setActive(service.key)}
+              className={`col-span-1 btn btn-outline text-white border-white ${active === service.key && 'bg-white !text-black'} hover:bg-white hover:text-black rounded-full btn-xs lg:btn-md text-sm lg:text-base px-4 py-2 lg:!px-5 lg:!py-0 h-auto`}
             >
               {service.name}
             </button>
           ))}
         </div>
         <div className="hidden lg:flex flex-col gap-5 lg:gap-5 w-full lg:w-3/4 mt-4">
-          {our_services[active].service.map((elm, index) => (
+          {our_services[index].service.map((elm, index) => (
             <div key={elm.name + index} className="flex flex-col  text-white ">
               <div className="flex items-center">
                 <p className={`text-base lg:text-2xl ${coconatDemi.className}`}>
@@ -56,7 +60,7 @@ export default function ServicePage() {
         </div>
         {/* mobile */}
         <div className="lg:hidden flex flex-col gap-5 lg:gap-10 w-full lg:w-3/4 mt-10">
-          {our_services[active].service.map((elm, index) => (
+          {our_services[index].service.map((elm, index) => (
             <div
               key={elm.name + index}
               className="flex flex-col gap-4 text-white "
--- src/components/Sections/Service.tsx
+++ src/components/Sections/Service.tsx
@@ -1,107 +1,52 @@
 'use client'
-import React from 'react'
-import { useTranslations } from 'next-intl'
 import Image from 'next/image'
+import { useTranslations } from 'next-intl'
+import { useRouter } from 'next/navigation'
 
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/16/solid'
+import { our_services } from '@/constants'
+import { coconatDemi, coconatBold } from '@/configs/fonts'
+import service_1 from '@/static/images/service-1.png'
 
-import Banner from '@/static/images/appointment.png'
-import { coconatBold, coconatDemi, coconatRegular } from '@/configs/fonts'
-
-function Appointment() {
+function Service() {
   const t = useTranslations()
-
+  const { push } = useRouter()
   return (
     <div
-      id="book_now"
-      className="flex flex-col gap-14 w-full items-center justify-center py-9 lg:py-[80px] px-4 lg:px-14 h-auto lg:h-screen lg:shrink-0 lg:snap-start"
+      id="service"
+      className="justify-center items-center flex flex-col py-9 lg:py-[75px] px-4 lg:px-14 w-full h-auto lg:h-screen lg:shrink-0 lg:snap-start"
     >
-      <div className="flex flex-col lg:grid lg:grid-cols-12 items-center lg:items-end gap-6 lg:gap-10 w-full h-full">
-        <h1
-          className={`text-center lg:hidden text-[28px] lg:text-5xl font-bold text-primary ${coconatBold.className}`}
-        >
-          {t('section.appointment.title')}
-        </h1>
-        <p className="text-center lg:hidden text-base lg:text-lg font-light lg:font-medium w-full">
-          {t('section.appointment.description')}
-        </p>
-        <Image
-          src={Banner}
-          className="h-72 lg:h-full w-full col-span-12 lg:col-span-7"
-          alt=""
-        />
-        <div className="lg:col-span-3 h-full flex flex-col lg:items-start lg:justify-end items-center text-center lg:text-start gap-6 w-full">
-          <h1
-            className={`text-center hidden lg:block text-[28px] lg:text-5xl font-bold uppercase text-primary ${coconatDemi.className}`}
+      <h1
+        className={`block lg:hidden text-center text-[28px] lg:text-5xl font-bold mb-5 text-primary ${coconatBold.className}`}
+      >
+        {t('section.service.title')}
+      </h1>
+      <h1
+        className={`hidden lg:block text-center text-[28px] lg:text-5xl font-bold mb-5 text-primary ${coconatDemi.className}`}
+      >
+        {t('section.service.title')}
+      </h1>
+      <p className="text-center text-base lg:text-lg font-light lg:font-medium w-full lg:w-[50%]">
+        {t('section.service.description')}
+      </p>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 items-center justify-center w-full mt-8 relative">
+        {our_services.map((item) => (
+          <div
+            key={item.name}
+            className="service cursor-pointer col-span-1 flex flex-col items-center text-center lg:flex-col gap-5 py-5 hover:text-primary"
+            onClick={() => push(`/service?tab=${item.key}`)}
           >
-            {t('section.appointment.title')}
-          </h1>
-          <p className="text-base hidden lg:block lg:text-lg font-light lg:font-medium w-full">
-            {t('section.appointment.description')}
-          </p>
-          <div className="flex flex-col">
-            <p
-              className={`${coconatRegular.className} text-black text-[28px] font-normal lg:text-left uppercase`}
+            <Image src={service_1} alt="" className={`h-1/4 w-full`} />
+            <span
+              className={`text-xl lg:text-2xl uppercase text-primary ${coconatDemi.className}`}
             >
-              {t('section.appointment.address.title')}
-            </p>
-            <p className="text-lg text-[28px] font-light lg:text-left">
-              {t('section.appointment.address.content')}
-            </p>
-            <p className="text-lg text-[28px] font-light lg:text-left">
-              {t('section.appointment.address.content_2')}
-            </p>
+              {item.name}
+            </span>
+            <p className="text-xl">{item.desc}</p>
           </div>
-          <div className="flex flex-col">
-            <p
-              className={`${coconatRegular.className} text-black text-[28px] font-normal lg:text-left uppercase`}
-            >
-              {t('section.appointment.contact.title')}
-            </p>
-            <p className="text-lg text-[28px] font-light lg:text-left">
-              {t('section.appointment.contact.hotline')}
-            </p>
-            <p className="text-lg text-[28px] font-light lg:text-left">
-              {t('section.appointment.contact.email')}
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <p
-              className={`${coconatRegular.className} text-black text-[28px] font-normal lg:text-left uppercase`}
-            >
-              {t('section.appointment.open_time.title')}
-            </p>
-            <p className="text-lg text-[28px] font-light lg:text-left">
-              {t('section.appointment.open_time.day')}
-            </p>
-            <p className="text-lg text-[28px] font-light lg:text-left">
-              {t('section.appointment.open_time.time')}
-            </p>
-          </div>
-          <button
-            onClick={() =>
-              window.open(
-                'https://brelaxmassage.com/bk/',
-                '_blank',
-                'noopener,noreferrer',
-              )
-            }
-            className="btn text-base uppercase  hover:bg-inherit text-white hover:text-white hover:bg-primary bg-primary border-0 rounded-full py-3 !px-16 lg:w-auto"
-          >
-            {t('book_now')}
-          </button>
-        </div>
-        <div className="flex flex-row gap-4">
-          <button className=" p-1 bg-white text-primary rounded-full border-[1px] border-black">
-            <ChevronLeftIcon className="w-8 h-8 font-bold" />
-          </button>
-          <button className=" p-1 bg-white text-primary rounded-full border-[1px] border-black">
-            <ChevronRightIcon className="w-8 h-8 font-bold" />
-          </button>
-        </div>
+        ))}
       </div>
     </div>
   )
 }
 
-export default Appointment
+export default Service
